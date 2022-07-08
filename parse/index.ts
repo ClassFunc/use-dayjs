27a4a0a8a4e860a361b4d3d67b8832879215f6da
damduong <duong.dam@classfunc.com>
2022-07-08 11:50:04 +0700

    add djsClone & some for parse component

--- parse/index.ts
+++ parse/index.ts
@@ -1,27 +1,54 @@
-import dayjs from "dayjs"
+import dayjs, {ConfigType} from "dayjs"
 import utc from "dayjs/plugin/utc";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import objectSupport from "dayjs/plugin/objectSupport";
 
+dayjs.extend(objectSupport)
+dayjs.extend(customParseFormat)
 dayjs.extend(utc)
 
-const djsParse = (aTime: string) => {
+const djsParse = (aTime: ConfigType) => {
     return dayjs(aTime)
 }
 
-const djsParseNow = () => {
-    return dayjs()
-}
-
-const djsParseUTC = (aTime: string) => {
+const djsParseUTC = (aTime: ConfigType) => {
     return dayjs(aTime).utc()
 }
 
-const djsParseNowUTC = () => {
+const djsNowUTC = () => {
     return dayjs().utc()
+}
+
+const djsNow = () => {
+    return dayjs()
+}
+
+const djsParseNow = () => {
+    return djsParse(djsNow().toString())
+}
+
+const djsParseNowUTC = () => {
+    return djsParse(djsNow().toString())
+}
+
+const djsUnix = (timestamp: number) => {
+    return dayjs.unix(timestamp)
+}
+
+const djsClone = (aTime?: ConfigType) => {
+    if (!aTime)
+        return dayjs(dayjs())
+
+    return dayjs(dayjs(aTime))
 }
 
 export {
     djsParse,
     djsParseNow,
     djsParseUTC,
-    djsParseNowUTC
+    djsParseNowUTC,
+    djsNow,
+    djsNowUTC,
+    djsUnix,
+    djsClone
 }
